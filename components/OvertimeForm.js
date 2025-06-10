d4fc9439513c1811514055d1b55bb3efc32f9471
MarlonMoe23 <marlon.moe24@gmail.com>
2025-06-09 23:49:37 -0500

    de minuto a minitp

--- components/OvertimeForm.js
+++ components/OvertimeForm.js
@@ -440,7 +440,7 @@ export default function OvertimeForm() {
                     onChange={(date) => setEndTime(date)}
                     showTimeSelect
                     timeFormat="HH:mm"
-                    timeIntervals={5}
+                    timeIntervals={1}
                     dateFormat="dd/MM/yyyy HH:mm"
                     className={`shadow border rounded w-full py-2 px-3 text-gray-700 ${errorStartEnd ? 'border-red-500' : ''}`}
                     maxDate={new Date()}
